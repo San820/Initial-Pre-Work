06cffa9de4234bc9702e4ca65b991d6d1c67545b
Santana <tooks.santana@gmail.com>
2019-05-19 15:54:51 -0600

    Access array data with indexes

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+// I am learning more about Array's which are list like objects. The index specify the entry into the array
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+
+var myData = myArray[0]
