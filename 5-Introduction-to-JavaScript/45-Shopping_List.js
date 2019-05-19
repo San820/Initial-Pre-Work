581ca92f282ae68487323f7b7f26692da594de2f
Santana <tooks.santana@gmail.com>
2019-05-19 15:56:20 -0600

    Shopping list

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,9 @@
+// I am learning how to create a list variable by nesting arrays
+var myList = [
+    ["Apple", 4],
+    ["Orange", 5],
+    ["Banana", 6],
+    ["Pineapple", 7],
+    ["Grape", 8]
+
+];
