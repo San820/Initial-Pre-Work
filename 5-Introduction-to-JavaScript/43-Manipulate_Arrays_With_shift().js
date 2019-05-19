7202076badef316a149f087240aaeb98c9e7fe4b
Santana <tooks.santana@gmail.com>
2019-05-19 15:55:59 -0600

    .shift()

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,11 @@
+// I am learning how to manipulate data within the array using the. pop(removes last element) and .shift(removes 1st element) functions.
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
