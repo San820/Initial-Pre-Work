8c6f38338a0801f18de50bee72f9e01b7d558edb
Santana <tooks.santana@gmail.com>
2019-05-19 15:56:10 -0600

    .unshift()

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,14 @@
+// The .unshift() function adds elements to the front of the array while .push() adds elements to the end.
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+
+myArray.unshift(["Paul", 35]);
