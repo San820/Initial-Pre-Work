0da10b14781de82d889acc882140ef13bdc7304a
Santana <tooks.santana@gmail.com>
2019-05-19 18:09:45 -0600

    String immutability

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// Individual String characters can't be altered once created. The only way is to re-assign the variable.
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
