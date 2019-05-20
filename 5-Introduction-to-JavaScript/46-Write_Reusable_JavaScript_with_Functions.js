b7da32871d5c83fa545f6f55dfa5e2f0c9883df9
Santana <tooks.santana@gmail.com>
2019-05-19 18:12:59 -0600

    Reusable JavaScript w/Functions

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+// I am learning how to create functions in java script and how to call functions by writing the function name followed by parenthesis
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+ 
+// Only change code below this line
+
+function reusableFunction () {
+  console.log("Hi World");
+}
+reusableFunction();
