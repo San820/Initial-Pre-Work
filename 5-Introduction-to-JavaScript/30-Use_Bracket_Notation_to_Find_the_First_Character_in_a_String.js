7b3c5023e91a4b7d4457217c4fcc17903bb63f09
Santana <tooks.santana@gmail.com>
2019-05-19 18:09:29 -0600

    Find the first character in a string

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,13 @@
+/* I am learning about bracket notation and how to display a specific character within a string. [0] is the first character in the string */
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
