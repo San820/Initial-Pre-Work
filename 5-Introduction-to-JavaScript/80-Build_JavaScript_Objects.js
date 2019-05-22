5052efc0db041f9d9124c332de0a53a8d16da19a
Santana <tooks.santana@gmail.com>
2019-05-22 10:56:37 -0600

    Build JavaScript objects

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// I am learning how build objects in JavaScript and how to store data within those objects
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  name: "Tommy",
+  legs: 4,
+  tails: 1,
+  friends: ["San"]
+
+};
