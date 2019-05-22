7bb1a637141e437369dc3c8d5327ce43dd85f4c8
Santana <tooks.santana@gmail.com>
2019-05-22 10:57:04 -0600

    Accessing objects with dot notation

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+// Accessing properties in .dot notation (var hatValue = testObj.hat) is used when you know the name of the property you want to access.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
