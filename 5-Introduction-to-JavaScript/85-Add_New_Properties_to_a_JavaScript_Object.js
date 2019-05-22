edb51e74c680dcc9db791e652621be3c20923f66
Santana <tooks.santana@gmail.com>
2019-05-22 10:58:40 -0600

    Adding new properties to objects

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// Adding properties to objects using dot notation is convenient and easy to read. Similar to updating both dot and bracket notation can be used to add objects
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.bark = "whoof!";
